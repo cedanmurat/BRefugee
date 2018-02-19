9a347849f5df1027d81194a9d332d329b5ea802a
tuba <uygunerlituba@gmail.com>
2018-02-19 16:39:19 +0200

    this signup screen

--- app/src/main/java/org/beyondrefuge/www/SignUp.java
+++ app/src/main/java/org/beyondrefuge/www/SignUp.java
@@ -1,19 +1,87 @@
 package org.beyondrefuge.www;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthActionCodeException;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class SignUp extends AppCompatActivity {
-    @BindView(R.id.emailsignup) EditText firstEditText;
-    @BindView(R.id.passwordsignup) EditText secondEditText;
+    @BindView(R.id.emailsignup) EditText emailEditText;
+    @BindView(R.id.passwordsignup) EditText passwordEditText;
+    @BindView(R.id.confirmation) EditText confirmationEditText;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        ButterKnife.bind(this);
+        mAuth=FirebaseAuth.getInstance();
+
+
     }
+    @OnClick(R.id.createAccount)
+    public  void createAccount(View view){
+    String email=emailEditText.getText().toString().trim();
+    String password=passwordEditText.getText().toString().trim();
+    String confirmation=confirmationEditText.getText().toString().trim();
+    if(email.isEmpty()){
+        emailEditText.setError("Email is required");
+        emailEditText.requestFocus();
+        return;
+    }
+    if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        emailEditText.setError("Please enter a valid email");
+        emailEditText.requestFocus();
+        return;
+    }
+    if(password.isEmpty()){
+        passwordEditText.setError("Password is required");
+        passwordEditText.requestFocus();
+        return;
+    }
+    if(password.length()<6){
+        passwordEditText.setError("Minimum length of password should be 6");
+        passwordEditText.requestFocus();
+        return;
+    }
+    if (!confirmation.equals(password)){
+        confirmationEditText.setError("Please check your password");
+
+    }
+    mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()){
+                Toast.makeText(getApplicationContext(),"User registered successfull",Toast.LENGTH_SHORT).show();
+            }
+            else {
+                if(task.getException() instanceof FirebaseAuthUserCollisionException){
+                    Toast.makeText(getApplicationContext(), "You are already registered", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(getApplicationContext(),task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    });
+    }
+
+
 }
