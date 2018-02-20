3fd2a55daa532852491739a40337378dd3545ef6
Cedan <cmzeynepli@gmail.com>
2018-02-20 18:09:18 +0300

    Facebook Login

--- app/src/main/java/org/beyondrefuge/www/Login.java
+++ app/src/main/java/org/beyondrefuge/www/Login.java
@@ -4,88 +4,162 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 
-public class Login extends AppCompatActivity implements View.OnClickListener{
+public class Login extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.emailLogin) EditText emailEditText;
     @BindView(R.id.passLogin) EditText passwordEditText;
-    Button btnLogin,btnSignUp;
+    private Button  btnSignUp;
+    private LoginButton loginButton;
     private FirebaseAuth mAuth;
+    CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mAuth=FirebaseAuth.getInstance();
-        emailEditText=(EditText)findViewById(R.id.emailLogin);
-        passwordEditText=(EditText)findViewById(R.id.passLogin);
-
+        mAuth = FirebaseAuth.getInstance();
         findViewById(R.id.login).setOnClickListener(this);
-        btnSignUp=(Button)findViewById(R.id.buttonsignup);
+        btnSignUp = (Button) findViewById(R.id.buttonsignup);
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(Login.this,SignUp.class);
+                Intent intent = new Intent(Login.this, SignUp.class);
                 startActivity(intent);
+            }
+        });
+        callbackManager=CallbackManager.Factory.create();
+        loginButton=(LoginButton)findViewById(R.id.facebook_login_button);
+        loginButton.setReadPermissions("email", "public_profile");
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(Login.this, "Login Cancelled", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(Login.this, "There is something wrong.", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
-public void useerLogin(){
 
-    String email=emailEditText.getText().toString().trim();
-    String password=passwordEditText.getText().toString().trim();
-    if(email.isEmpty()){
-        emailEditText.setError("Email is required");
-        emailEditText.requestFocus();
-        return;
-    }
-    if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-        emailEditText.setError("Please enter a valid email");
-        emailEditText.requestFocus();
-        return;
-    }
-    if(password.isEmpty()){
-        passwordEditText.setError("Password is required");
-        passwordEditText.requestFocus();
-        return;
-    }
-    if(password.length()<6){
-        passwordEditText.setError("Minimum length of password should be 6");
-        passwordEditText.requestFocus();
-        return;
-    }
-    mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-        @Override
-        public void onComplete(@NonNull Task<AuthResult> task) {
-            if (task.isSuccessful()){
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-            }else {
-                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-            }
+        // Pass the activity result back to the Facebook SDK
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser!=null){
+            startActivity(new Intent());
+            finish();
         }
-    });
+    }
+    public void userLogin() {
 
-}
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+        if (email.isEmpty()) {
+            emailEditText.setError("Email is required");
+            emailEditText.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.setError("Please enter a valid email");
+            emailEditText.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            passwordEditText.setError("Password is required");
+            passwordEditText.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            passwordEditText.setError("Minimum length of password should be 6");
+            passwordEditText.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+
+                } else {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
 
     @Override
     public void onClick(View view) {
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.login:
-                useerLogin();
+                userLogin();
                 break;
 
+        }
     }
-}
+    private void handleFacebookAccessToken(AccessToken token) {
+
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+    }
+
 }
