eb7af18daba8b4a92f11d75652a234c9ee13a657
Cedan <cmzeynepli@gmail.com>
2018-02-22 00:23:15 +0300

    "Nav Drawer and Fragments"

---
+++ app/src/main/java/org/beyondrefuge/www/Fragments/AboutUs.java
@@ -0,0 +1,29 @@
+package org.beyondrefuge.www.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.beyondrefuge.www.R;
+
+/**
+ * Created by Recoded Cedan on 21.02.2018.
+ */
+
+public class AboutUs extends Fragment {
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.aboutus,null);
+    }
+}
+
