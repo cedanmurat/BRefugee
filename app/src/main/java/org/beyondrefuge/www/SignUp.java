3b87764edcdf225110a4da7e39bba5c77c2d42a6
Cedan <cmzeynepli@gmail.com>
2018-03-05 17:36:50 +0300

    fixing adapters and videoactivity

--- app/src/main/java/org/beyondrefuge/www/SignUp.java
+++ app/src/main/java/org/beyondrefuge/www/SignUp.java
@@ -1,9 +1,16 @@
 package org.beyondrefuge.www;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -46,6 +53,8 @@ public class SignUp extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         ButterKnife.bind(this);
+        CheckingInternet();
+
         mAuth = FirebaseAuth.getInstance();
 
 
@@ -119,6 +128,36 @@ public class SignUp extends AppCompatActivity {
 
         mReference.child("users").child(userId).setValue(user);
     }
-    
+
+    protected boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnectedOrConnecting()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void CheckingInternet() {
+        if (isOnline()) {
+
+        } else {
+            new AlertDialog.Builder(SignUp.this)
+                    .setTitle("Internet Connection")
+                    .setMessage("There isn't Internet connection. \n Please open your connection.")
+                    .setCancelable(false)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                             startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+
+                        }
+                    }).show();
+        }
+
+    }
+
 
 }
